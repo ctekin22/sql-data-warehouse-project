f3fa27cf96611d88357d7f3fe7f8cc53ff20c24f
Cnscn <ctekin22@gmail.com>
2025-06-19 13:54:23 -0500

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -41,4 +41,48 @@ CREATE VIEW gold.dim_customer AS
         LEFT JOIN silver.erp_loc_a101 la ON ci.cst_key = la.cid;
 
 
+-- =============================================================================
+-- Create Dimension: gold.dim_product
+-- =============================================================================
+IF OBJECT_ID('gold.dim_product', 'V') IS NOT NULL
+    DROP VIEW gold.dim_product;
+GO
 
+CREATE VIEW gold.dim_product AS
+    SELECT 
+        ROW_NUMBER() OVER(ORDER BY pi.prd_start_dt, pi.prd_key) AS product_key,
+        pi.prd_id AS product_id,
+        pi.prd_key AS product_number,
+        pi.prd_nm AS product_name,
+        pi.cat_id AS category_id,
+        cat.cat AS category,
+        cat.subcat AS subcategory,
+        cat.maintance,
+        pi.prd_cost AS cost,
+        pi.prd_line AS product_line,
+        pi.prd_start_dt AS start_date
+    FROM silver.crm_prd_info pi
+    LEFT JOIN silver.erp_px_cat_g1v2 cat ON pi.cat_id = cat.id
+    WHERE pi.prd_end_dt IS NULL;
+
+-- =============================================================================
+-- Create Dimension: gold.fact_sales
+-- =============================================================================
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+    DROP VIEW gold.fact_sales;
+GO
+
+CREATE VIEW gold.fact_sales AS 
+    SELECT
+        sa.sls_ord_num AS order_number,
+        pc.product_key,
+        dc.customer_key,
+        sa.sls_order_dt AS order_date,
+        sa.sls_ship_dt AS shipping_date,
+        sa.sls_due_dt AS due_date,
+        sa.sls_sales AS sales,
+        sa.sls_quantity AS quantity,
+        sa.sls_price AS price
+    FROM silver.crm_sales_details sa 
+    LEFT JOIN gold.dim_customer dc ON sa.sls_cust_id = dc.customer_id 
+    LEFT JOIN gold.dim_product pc ON sa.sls_prd_key = pc.product_number;
